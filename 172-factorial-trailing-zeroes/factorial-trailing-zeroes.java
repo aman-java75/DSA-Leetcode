10175221db35f79f2e5818345b817da0a8504ace
aman-java75 <aman.ks.java75@gmail.com>
2026-09-22 19:55:32 +0530

    Time: 0 ms (100.00%) | Memory: 42.4 MB (34.60%) - LeetSync

--- 172-factorial-trailing-zeroes/factorial-trailing-zeroes.java
+++ 172-factorial-trailing-zeroes/factorial-trailing-zeroes.java
@@ -1,19 +1,11 @@
 class Solution {
     public int trailingZeroes(int n) {
-        int countOf2 = 0;
-        int countOf5 = 0;
-
-        for(int i=1;i<=n;i++){
-            int num = i;
-            while(num%2==0){
-                countOf2++;
-                num = num/2;
-            }
-            while(num%5==0){
-                countOf5++;
-                num = num/5;
-            }
+        int count = 0;
+        int i = 5;
+        while(i<=n){
+            count = count + n/i;
+            i = i*5;
         }
-        return Math.min(countOf2,countOf5);
+        return count;
     }
 }
